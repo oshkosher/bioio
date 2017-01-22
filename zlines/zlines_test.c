86cdcac6f1d26e658c98a11b22af940feec46acf
Ed L. Karrels <ed.karrels@gmail.com>
2017-01-22 13:04:57 -0600

    add comments, simplify datatypes

--- zlines/zlines_test.c
+++ zlines/zlines_test.c
@@ -5,13 +5,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
 #include "zline_api.h"
 
 
 int main(int argc, char **argv) {
   ZlineFile *zf;
-  uint64_t i, count;
+  long i, count;
   char *line = NULL;
 
   if (argc != 2) {
@@ -19,21 +18,28 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  /* open the file */
   zf = ZlineFile_read(argv[1]);
   if (!zf) {
     printf("Failed to open %s\n", argv[1]);
     return 1;
   }
 
+  /* get the number of lines in the file */
   count = ZlineFile_line_count(zf);
+
+  /* allocate a buffer big enough to hold any line */
   line = (char*) malloc(ZlineFile_max_line_length(zf) + 1);
+
+  /* extract each line (but don't do anything with it) */
   for (i = 0; i < count; i++) {
     ZlineFile_get_line(zf, i, line);
   }
+
   free(line);
   ZlineFile_close(zf);
 
-  printf("%" PRIu64 " lines read.\n", count);
+  printf("%ld lines read.\n", count);
 
   return 0;
 }
