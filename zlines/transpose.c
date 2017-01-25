f261fa0f99b84b190f4a05443c0ab7aa3722444d
Ed K <edk@illinois.edu>
2017-01-25 17:24:08 -0600

    change transpose from mmap to read/write

--- zlines/transpose.c
+++ zlines/transpose.c
@@ -36,6 +36,7 @@ transpose 1999999x21946 = 43891978054 bytes in 1084.340s at 38.6 MiB/s
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <limits.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -50,11 +51,31 @@ typedef struct {
   int n_rows, n_cols, row_stride;
 } Array2d;
 
-Array2d in, out;
-
 /* Get a pointer to a character in an Array2d. 'a' should be an Array2d,
    not a pointer to one. */
 #define Array2d_ptr(a, row, col) ((a).data + (u64)(row) * (a).row_stride + (col))
+
+typedef struct {
+  int fd;
+  const char *filename;
+  int n_rows, n_cols, row_stride, newline_type;
+} File2d;
+
+/* If for_writing is nonzero, n_rows, n_cols, row_stride, and newline_type
+   will need to be set before calling this. If it's zero, then they will
+   be filled in. */
+int File2d_open(File2d *f, const char *filename, int for_writing);
+void File2d_close(File2d *f);
+
+/* Find the first instance of the given byte in the file */
+int File2d_find_byte(File2d *f, char c);
+
+/* Get the offset of a location in a File2d */
+u64 File2d_offset(File2d *f, int row, int col);
+
+File2d in_file, out_file;
+Array2d in, out;
+double time_reading=0, time_transposing=0, time_writing=0;
 
 /* If nonzero, blocks read will progress to the right, then down, and
    blocks written will progress down, then right.
@@ -71,8 +92,7 @@ int read_block_width = 256;
 
 int cache_ob_size = 128;
 
-int default_temp_width = 4096;
-int default_temp_height = 4096;
+int default_temp_size = 4096;
 
 #define NEWLINE_UNIX 1
 #define NEWLINE_DOS 2
@@ -95,6 +115,8 @@ void transpose
 (Array2d *dest, int dest_row, int dest_col,
  Array2d *src, int src_row, int src_col,
  int height, int width);
+
+void transposeFiles(File2d *out, File2d *in);
 
 /* Transpose large chunks from the input file into memory, then copy them
    to the output file. */
@@ -127,15 +149,32 @@ void copy2d
  Array2d *src, int src_row, int src_col,
  int height, int width);
 
+void copy2dToFile
+(File2d *dest, int dest_row, int dest_col,
+ Array2d *src, int src_row, int src_col,
+ int height, int width);
+
+void copy2dFromFile
+(Array2d *dest, int dest_row, int dest_col,
+ File2d *src, int src_row, int src_col,
+ int height, int width);
+
+
 
 int main(int argc, char **argv) {
   int result = 0;
-  int newline_len;
-  u64 in_file_len, out_file_len = 0, byte_count;
+  /* int newline_len; */
+  u64 /* out_file_len = 0,*/ byte_count;
   double start_time, elapsed, mbps;
 
   if (argc != 3) printHelp();
-  
+
+  if (File2d_open(&in_file, argv[1], 0)) {
+    fprintf(stderr, "Failed to open %s\n", argv[1]);
+    return 1;
+  }
+    
+  /*
   if (mapFile(argv[1], 0, (char**)&in.data, &in_file_len)) {
     fprintf(stderr, "Failed to open %s\n", argv[1]);
     return 1;
@@ -143,40 +182,49 @@ int main(int argc, char **argv) {
 
   if (getFileDimensions(&in, in_file_len, &newline_type))
     goto fail;
-
-  newline_len = newlineLength(newline_type);
-  out.n_cols = in.n_rows;
-  out.n_rows = in.n_cols;
-  in.row_stride = in.n_cols + newline_len;
-  out.row_stride = out.n_cols + newline_len;
+  */
 
   printf("%s has %d rows of length %d with %s line endings\n",
-         argv[1], in.n_rows, in.n_cols, newlineName(newline_type));
+         in_file.filename, in_file.n_rows, in_file.n_cols,
+         newlineName(in_file.newline_type));
 
-  out_file_len = (u64)out.n_rows * out.row_stride;
+  out_file.n_rows = in_file.n_cols;
+  out_file.n_cols = in_file.n_rows;
+  out_file.newline_type = in_file.newline_type;
 
+  /* out_file_len = (u64)out.n_rows * out.row_stride; */
+
+  /*
   if (mapFile(argv[2], 1, &out.data, &out_file_len)) {
+    fprintf(stderr, "Failed to open %s\n", argv[2]);
+    return 1;
+  }
+  */
+
+  if (File2d_open(&out_file, argv[2], 1)) {
     fprintf(stderr, "Failed to open %s\n", argv[2]);
     return 1;
   }
 
   start_time = getSeconds();
 
-  byte_count = (u64)in.n_rows * in.n_cols;
+  byte_count = (u64)in_file.n_rows * in_file.n_cols;
 
-  transposeTwoStage(&out, 0, 0, &in, 0, 0, in.n_rows, in.n_cols);
+  transposeFiles(&out_file, &in_file);
+  /* transposeTwoStage(&out, 0, 0, &in, 0, 0, in.n_rows, in.n_cols); */
 
   putchar('\n');
   
   elapsed = getSeconds() - start_time;
   mbps = byte_count / (elapsed * 1024 * 1024);
   printf("transpose %dx%d = %" PRIu64" bytes in %.3fs at %.1f MiB/s\n",
-         in.n_rows, in.n_cols, byte_count, elapsed, mbps);
+         in_file.n_rows, in_file.n_cols, byte_count, elapsed, mbps);
 
-
+  /*
  fail:
   if (in.data) munmap(in.data, in_file_len);
   if (out.data) munmap(out.data, out_file_len);
+  */
   
   return result;
 }
@@ -245,6 +293,11 @@ int getFileDimensions(Array2d *array, u64 length, int *newline_type) {
   return 0;
 }
 
+/* Get the offset of a location in a File2d */
+u64 File2d_offset(File2d *f, int row, int col) {
+  return (u64)row * f->row_stride + col;
+}
+
 
 void writeNewline(char *dest, int newline_type) {
   if (newline_type == NEWLINE_DOS) {
@@ -264,19 +317,19 @@ void writeTileNewlines(int block_top, int block_bottom) {
 
 
 int createTempBuffer(Array2d *temp) {
-  u64 temp_size = (u64)default_temp_width * default_temp_height;
+  u64 temp_size = (u64)default_temp_size * default_temp_size;
 
   assert(in.n_cols > 0 && in.n_rows > 0);
 
-  if (in.n_cols < default_temp_height) {
+  if (in.n_cols < default_temp_size) {
     temp->n_rows = in.n_cols;
     temp->n_cols = MIN(in.n_rows, temp_size / in.n_cols);
-  } else if (in.n_rows < default_temp_width) {
+  } else if (in.n_rows < default_temp_size) {
     temp->n_cols = in.n_rows;
     temp->n_rows = MIN(in.n_cols, temp_size / in.n_rows);
   } else {
-    temp->n_rows = default_temp_height;
-    temp->n_cols = default_temp_width;
+    temp->n_rows = default_temp_size;
+    temp->n_cols = default_temp_size;
   }
 
   printf("temp buffer %dx%d\n", temp->n_rows, temp->n_cols);
@@ -325,6 +378,74 @@ void transpose
     }
   }
 }
+
+
+void transposeFiles(File2d *out_file, File2d *in_file) {
+  Array2d in, out;
+  int row, x, y, block_width, block_height;
+  double start_time, elapsed;
+
+  in.n_rows = out.n_cols = in.n_cols = out.n_rows = in.row_stride =
+    default_temp_size;
+
+  /* allocate an extra column or two for 'out', so it can efficiently write
+     newlines to the output file */
+  out.row_stride = default_temp_size + newlineLength(out_file->newline_type);
+
+  /* allocate data for an input buffer and output buffer */
+  in.data = (char*)malloc(in.n_rows * in.row_stride);
+  out.data = (char*)malloc(out.n_rows * out.row_stride);
+  assert(in.data && out.data);
+
+  /* read blocks of the input file, transpose them, and write blocks to
+     the output file. */
+  for (x = 0; x < in_file->n_cols; x += in.n_cols) {
+    block_width = MIN(in.n_cols, in_file->n_cols - x);
+
+    for (y = 0; y < in_file->n_rows; y += in.n_rows) {
+      block_height = MIN(in.n_rows, in_file->n_rows - y);
+
+      /* read a block into input buffer */
+      start_time = getSeconds();
+      copy2dFromFile(&in, 0, 0, in_file, y, x, block_height, block_width);
+      elapsed = getSeconds() - start_time;
+      time_reading += elapsed;
+      /* printf("read %dx%d block %.3fs\n", block_height, block_width, elapsed); */
+
+      /* transpose that block into the output buffer */
+      start_time = getSeconds();
+      transpose(&out, 0, 0, &in, 0, 0, block_height, block_width);
+      elapsed = getSeconds() - start_time;
+      time_transposing += elapsed;
+      /* printf("  transpose %.3fs\n", elapsed); */
+
+      /* if this is the last block of the row, write the newlines as well. */
+      if (y + block_height == out_file->n_cols) {
+        for (row = 0; row < block_width; row++)
+          writeNewline(Array2d_ptr(out, row, block_height),
+                       out_file->newline_type);
+      
+        block_height += newlineLength(out_file->newline_type);
+      }
+
+      /* write the output buffer to the output file */
+      start_time = getSeconds();
+      copy2dToFile(out_file, x, y, &out, 0, 0, block_width, block_height);
+      elapsed = getSeconds() - start_time;
+      time_writing += elapsed;
+      /* printf("  write %.3fs\n", elapsed); */
+
+    }
+  }
+
+  free(in.data);
+  free(out.data);
+
+  printf("\nread time %.3fs, in-memory transpose time %.3fs, write time %.3fs\n",
+         time_reading, time_transposing, time_writing);
+}
+  
+  
 
 
 /* Transpose large chunks from the input file into memory, then copy them
@@ -404,8 +525,10 @@ void transposeTile(Array2d *dest, int dest_row, int dest_col,
   bytes_done += (u64)width * height;
   if (bytes_done > next_report) {
     char buf1[50], buf2[50];
-    printf("\r%s of %s bytes done", commafy(buf1, bytes_done), 
-           commafy(buf2, (u64)in.n_rows * in.n_cols));
+    printf("\r%s of %s bytes done, r=%.3f tx=%.3f w=%.3f",
+           commafy(buf1, bytes_done), 
+           commafy(buf2, (u64)in_file.n_rows * in_file.n_cols),
+           time_reading, time_transposing, time_writing);
     fflush(stdout);
     next_report += 100*1000*1000;
   }
@@ -490,5 +613,166 @@ void copy2d
       writeNewline(Array2d_ptr(*dest, dest_row + row, dest->n_cols),
                    newline_type);
   }
+}
+
+
+void copy2dToFile
+(File2d *dest, int dest_row, int dest_col,
+ Array2d *src, int src_row, int src_col,
+ int height, int width) {
+  int row;
+  u64 file_offset;
+
+  for (row = 0; row < height; row++) {
+    file_offset = File2d_offset(dest, dest_row + row, dest_col);
+    if (width != pwrite(dest->fd, Array2d_ptr(*src, src_row + row, src_col),
+                        width, file_offset)) {
+      fprintf(stderr, "Error writing %d bytes to %s at offset %" PRIu64 "\n",
+              width, dest->filename, file_offset);
+    }
+  }
+}
+  
+
+void copy2dFromFile
+(Array2d *dest, int dest_row, int dest_col,
+ File2d *src, int src_row, int src_col,
+ int height, int width) {
+  int row;
+  u64 file_offset;
+
+  for (row = 0; row < height; row++) {
+    file_offset = File2d_offset(src, src_row + row, src_col);
+    if (width != pread(src->fd, Array2d_ptr(*dest, dest_row + row, dest_col),
+                        width, file_offset)) {
+      fprintf(stderr, "Error read %d bytes from %s at offset %" PRIu64 "\n",
+              width, src->filename, file_offset);
+    }
+  }
+}
+
+int File2d_open(File2d *f, const char *filename, int for_writing) {
+  int flags;
+  mode_t mode = 0;
+  u64 length, first_line_len, row;
+  char buf[10];
+
+  if (for_writing) {
+    flags = O_RDWR | O_CREAT | O_TRUNC;
+    mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+    assert(f->n_cols && f->n_rows && f->newline_type);
+    f->row_stride = f->n_cols + newlineLength(f->newline_type);
+  } else {
+    flags = O_RDONLY;
+    length = getFileSize(filename);
+    if (length == 0) goto fail;
+  }
+  
+  f->filename = filename;
+  f->fd = open(filename, flags, mode);
+  if (f->fd == -1) goto fail;
+
+  if (for_writing) {
+    length = f->n_rows * f->row_stride;
+    if (ftruncate(f->fd, length)) {
+      fprintf(stderr, "Failed to set the length of %s to %" PRIu64 " bytes\n",
+              filename, length);
+      goto fail;
+    }
+  } else {
+    assert(length > 0);
+
+    /* guesstimate out the file dimensions */
+    first_line_len = File2d_find_byte(f, '\n');
+    if (first_line_len >= INT_MAX) {
+      fprintf(stderr, "First line of %s is %" PRIu64 " bytes, which is longer "
+              "than this tool supports.\n", filename, first_line_len);
+      goto fail;
+    }
+
+    if (first_line_len == length || first_line_len == -1) {
+      fprintf(stderr, "Invalid input file: no line endings found.\n");
+      goto fail;
+    }
+      
+
+    if (first_line_len == 0) {
+      fprintf(stderr, "Invalid input file: first line is empty\n");
+      goto fail;
+    }
+
+    /* check for DOS (0x0d 0x0a) line endings, since having two bytes at the
+       end of each line will change the row stride of the data */
+    pread(f->fd, buf, 1, first_line_len-1);
+    f->newline_type = (buf[0] == '\r') ? NEWLINE_DOS : NEWLINE_UNIX;
+
+    f->row_stride = first_line_len + 1;
+    f->n_cols = f->row_stride - newlineLength(f->newline_type);
+
+    /* For this tool to work correctly, every line of the file must have
+       the same length. Don't check every line, but check that the file
+       size is an integer multiple of the line length. */
+    
+    f->n_rows = length / f->row_stride;
+    if (length != f->n_rows * (u64)f->row_stride) {
+      fprintf(stderr, "Invalid input file: uneven line lengths "
+              "(rows appear to be %d bytes each, but that doesn't evenly "
+              "divide the file length, %" PRIu64 "\n",
+              f->row_stride, length);
+      goto fail;
+    }
+
+    /* Check for the presence of a newline character in the right place
+       in a few more rows. */
+
+    for (row=10; row < f->n_rows; row *= 10) {
+      /* printf("check row %d\n", (int)row); */
+      pread(f->fd, buf, 1, File2d_offset(f, row, f->row_stride-1));
+      if (buf[0] != '\n') {
+        fprintf(stderr, "Invalid input file: row %d length mismatch\n",
+                (int)row);
+        return -1;
+      }
+    }
+    
+  }
+
+  return 0;
+
+ fail:
+  if (f->fd >= 0) close(f->fd);
+  f->fd = -1;
+  return -1;
+}
+    
+
+/* Find the first instance of the given byte in the file */
+int File2d_find_byte(File2d *f, char c) {
+  char buf[4096], *found_pos;
+  u64 pos, original_pos =  lseek(f->fd, 0, SEEK_CUR);
+  int read_len;
+
+  pos = lseek(f->fd, 0, SEEK_SET);
+  assert(pos == 0);
+
+  do {
+    read_len = read(f->fd, buf, sizeof buf);
+    if (read_len < 0) return -1;
+    found_pos = memchr(buf, c, read_len);
+    if (found_pos) {
+      lseek(f->fd, original_pos, SEEK_SET);
+      return pos + (found_pos - buf);
+    }
+    pos += read_len;
+  } while (read_len == sizeof buf);
+
+  lseek(f->fd, original_pos, SEEK_SET);
+  return -1;
+}
+  
+  
+void File2d_close(File2d *f) {
+  close(f->fd);
+  f->fd = -1;
 }
 
