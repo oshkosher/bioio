3a63ae61af59d7610a41b39a69394ca18e8ed808
Ed K <ed.karrels@gmail.com>
2017-01-31 18:19:10 -0600

    snapshot checkin of transpose2.c, though it still has bugs

--- zlines/transpose2.c
+++ zlines/transpose2.c
@@ -36,13 +36,15 @@
 typedef uint64_t u64;
 
 #define DEFAULT_MEMORY_SIZE (32 * 1024 * 1024)
-#define VERBOSE 0
+#define IO_UPDATE_FREQUENCY (10*1024*1024)
+#define VERBOSE 1
 
 FILE *input_file, *output_file;
 u64 memory_size;
 const char *temp_directory;
 int input_width, input_height;  /* total input size */
-int read_buffer_rows_filled;
+int read_buffer_rows_filled = 0;
+int quiet;
 Array2d read_buffer, write_buffer;
 
 char *temp_file_name = NULL;
@@ -64,48 +66,68 @@ int tempFileWrite(int row, int col, const char *src, u64 len);
 int tempFileRead(int row, int col, char *dest, u64 len);
 int tempFileReadTx(int row, int col, char *dest, u64 len);
 int removeTempFile();
+u64 getBlockBytes();       /* bytes per block */
+int getTotalBlockCount();  /* number of blocks */
 
 int readFile(FILE *input_file);
 int addLine(const char *line, int line_no);
+int flushWriteBufferToTemp(int first_row);
 int transposeData();
 int writeFile(FILE *output_file);
 
 int main(int argc, char **argv) {
   double initial_time, start_time, elapsed, mbps;
   u64 file_size;
+  int result = 0;
 
   if (parseArgs(argc, argv)) return 1;
 
+  /* read the file */
   initial_time = start_time = getSeconds();
-  if (readFile(input_file)) return 1;
+  if (readFile(input_file)) goto fail;
   elapsed = getSeconds() - start_time;
   file_size = (u64)input_height * input_width;
   mbps = file_size / (1024*1024 * elapsed);
-  fprintf(stderr, "%dx%d=%" PRIu64 " bytes read in %.3fs, %.3f MiB/s\n",
-          input_height, input_width, file_size, elapsed, mbps);
+  if (!quiet) {
+    char buf[50];
+    fprintf(stderr, "%d x %d = %s bytes read in %.3fs, %.3f MiB/s\n",
+            input_height, input_width, commafy(buf, file_size), elapsed, mbps);
+  }
 
   if (input_file != stdin) fclose(input_file);
 
+  /* transpose the data */
   start_time = getSeconds();
-  if (transposeData()) return 1;
-  fprintf(stderr, "Transposed in %.3fs\n", getSeconds() - start_time);
+  if (transposeData()) goto fail;
+  if (!quiet) {
+    double elapsed = getSeconds() - start_time;
+    double mbps = file_size / (1024 * 1024 * elapsed);
+    fprintf(stderr, "Transpose time %.3fs, %.3f MiB/s\n", elapsed, mbps);
+  }
 
+  /* write the data */
   start_time = getSeconds();
-  if (writeFile(output_file)) return 1;
+  if (writeFile(output_file)) goto fail;
   if (output_file != stdout) fclose(output_file);
-  elapsed = getSeconds() - start_time;
-  mbps = file_size / (1024*1024 * elapsed);
-  fprintf(stderr, "Result written in %.3fs, %.3f MiB/s\n", elapsed, mbps);
 
+  if (!quiet) {
+    elapsed = getSeconds() - start_time;
+    mbps = file_size / (1024*1024 * elapsed);
+    fprintf(stderr, "Result written in %.3fs, %.3f MiB/s\n", elapsed, mbps);
+    elapsed = getSeconds() - initial_time;
+    mbps = file_size / (1024*1024 * elapsed);
+    fprintf(stderr, "Total time: %.3fs, %.3f MiB/s\n", elapsed, mbps);
+  }
+  goto done;
+
+ fail:
+  result = 1;
+ done:
   removeTempFile();
   deallocateBuffers();
 
-  fprintf(stderr, "Total time: %.3fs\n", getSeconds() - initial_time);
-  
-  return 0;
+  return result;
 }
-  
-
 
 
 void printHelp() {
@@ -118,6 +140,7 @@ void printHelp() {
           "   -m <size> : use this much memory for the buffer\n"
           "               k, m, g suffixes are recognized\n"
           "   -d <directory> : use this directory for temporary files\n"
+          "   -q : suppress status output\n"
           "\n");
   exit(1);
 }
@@ -130,7 +153,7 @@ int parseArgs(int argc, char **argv) {
   temp_directory = ".";
   input_file = stdin;
   output_file = stdout;
-
+  quiet = 0;
 
   if (isDirectory("/tmp")) {
     temp_directory = "/tmp";
@@ -150,6 +173,10 @@ int parseArgs(int argc, char **argv) {
         fprintf(stderr, "Invalid memory size: \"%s\"\n", argv[argno]);
         return 1;
       }
+    }
+      
+    else if (!strcmp(argv[argno], "-q")) {
+      quiet = 1;
     }
       
     else if (!strcmp(argv[argno], "-d")) {
@@ -208,6 +235,7 @@ int readFile(FILE *input_file) {
   size_t line_buf_capacity = 0;
   ssize_t line_len;
   int result = 0, line_no = 0;
+  u64 bytes_read = 0, next_update = IO_UPDATE_FREQUENCY;
 
   input_width = -1;
   input_height = -1;
@@ -215,6 +243,14 @@ int readFile(FILE *input_file) {
   while (1) {
     line_len = getline(&line_buf, &line_buf_capacity, input_file);
     if (line_len == -1) break;
+    bytes_read += line_len;
+
+    if (!quiet && bytes_read >= next_update) {
+      char buf[50];
+      fprintf(stderr, "\r%d rows, %s bytes read",
+              line_no + 1, commafy(buf, bytes_read));
+      next_update = bytes_read + IO_UPDATE_FREQUENCY;
+    }
     
     if (line_no == INT_MAX) {
       fprintf(stderr, "Error: %" PRIu64 " lines read. "
@@ -247,6 +283,11 @@ int readFile(FILE *input_file) {
     }
 
     if (addLine(line_buf, line_no++)) goto fail;
+  }
+  
+  if (!quiet) {
+    /*fprintf(stderr, "\r%d rows, %" PRIu64 " bytes read\n", line_no, bytes_read); */
+    fputc('\r', stderr);
   }
 
   if (addLine(NULL, line_no)) goto fail;
@@ -296,8 +337,6 @@ int allocateBuffers() {
             read_buffer_height, read_buffer_width);
     return 1;
   }
-  fprintf(stderr, "Read buffer %d x %d\n",
-          read_buffer_height, read_buffer_width);
 
   if (Array2d_init(&write_buffer, read_buffer_width, read_buffer_height,
                    read_buffer_height)) {
@@ -305,8 +344,11 @@ int allocateBuffers() {
             read_buffer_width, read_buffer_height);
     return 1;
   }
-  fprintf(stderr, "Write buffer %d x %d\n",
-          read_buffer_width, read_buffer_height);
+  if (!quiet) {
+    fprintf(stderr, "Read buffer %d x %d, write buffer %d x %d\n",
+            read_buffer_height, read_buffer_width,
+            read_buffer_width, read_buffer_height);
+  }
 
   /* don't create a temp file yet */
   
@@ -322,34 +364,39 @@ void deallocateBuffers() {
 
 
 int insureTempFileExists() {
+  int temp_fd;
+
   if (temp_file) return 0;
 
   assert(temp_directory && !temp_file_name);
-
-  temp_file_name = strdup(tempnam(temp_directory, "transpose_tmp."));
-  fprintf(stderr, "temp file %s\n", temp_file_name);
-  
   /*
+  temp_file_name = tempnam(temp_directory, "transpose_tmp.");
+  */
+  
   temp_file_name = (char*)malloc(strlen(temp_directory) + 22);
   if (!temp_file_name) {
     fprintf(stderr, "Out of memory creating temp file.\n");
     return -1;
   }
-  sprintf(temp_file_name, "%s/transpose_tmp.XXXXXX", temp_directory);
+  snprintf(temp_file_name, strlen(temp_directory) + 22,
+           "%s/transpose_tmp.XXXXXX", temp_directory);
 
-  temp_fd = mkostemp(temp_file_name, O_EXCL|O_CREAT);
+  temp_fd = mkstemp(temp_file_name);
   if (temp_fd == -1) {
-    fprintf(stderr, "Failed to open temp file in 
-            temp_file_name);
+    fprintf(stderr, "Failed to open temp file in %s\n", temp_directory);
     return -1;
   }
-  */
-  
-  temp_file = fopen(temp_file_name, "w+");
+
+  temp_file = fdopen(temp_fd, "w+");
   if (!temp_file) {
-    fprintf(stderr, "Failed to create temp file %s\n", temp_file_name);
+    fprintf(stderr, "Failed to access temp file %s\n", temp_file_name);
     return -1;
   }
+
+  /*
+  if (!quiet)
+    fprintf(stderr, "Using temp file %s\n", temp_file_name);
+  */
 
   return 0;
 }
@@ -476,11 +523,59 @@ int removeTempFile() {
               temp_file_name, strerror(errno));
       return -1;
     }
-    free(temp_file_name);
   }
+  free(temp_file_name);
   return 0;
 }
 
+
+/* bytes per block */
+u64 getBlockBytes() {
+  return (u64) read_buffer.n_rows * read_buffer.n_cols;
+}
+
+  
+/* number of blocks */
+int getTotalBlockCount() {
+  static u64 block_count = 0;
+
+  assert(input_width > 0 && input_height > 0 && read_buffer.n_cols > 0 &&
+         read_buffer.n_rows > 0);
+
+  if (block_count) return block_count;
+
+  block_count = 
+    (input_width + read_buffer.n_cols - 1) / read_buffer.n_cols;
+  block_count *=
+    (input_height + read_buffer.n_rows - 1) / read_buffer.n_rows;
+
+  /* check for int overflow */
+  assert(block_count <= INT_MAX);
+
+  return (int)block_count;
+}
+
+
+/* There are 3 states:
+   1. The input data is narrower than the read buffer, and we haven't yet
+      read more rows of input data that what fit in the read buffer.
+      (input_width <= read_buffer.n_cols && line_no < read_buffer.n_rows)
+
+      Everything is still in memory. Append the new line.
+
+   2. The input data is narrower than the read buffer, but we've read more
+      lines than can fit in the read buffer.
+      (input_width <= read_buffer.n_cols && line_no >= read_buffer.n_rows)
+      
+      When the read buffer fills up, write the whole thing to the temp
+      file, a block at a time.
+
+   3. The input data is wider than the read buffer.
+      (input_width > read_buffer.n_cols)
+
+      On each input line, split it into sections of read buffer width,
+      and write each to the appropriate place in the temp file.
+*/
 int addLine(const char *line, int line_no) {
 
   /* when this is called with line==NULL, then all data has been read */
@@ -488,29 +583,33 @@ int addLine(const char *line, int line_no) {
     input_height = line_no;
 
     if (temp_file) {
-      /* flush the buffer, if a temp file has been created */
-      if (read_buffer_rows_filled) {
-        if (tempFileWrite(line_no - read_buffer_rows_filled,
-                          0, read_buffer.data,
-                          (u64)read_buffer_rows_filled * read_buffer.n_cols))
-        return -1;
-        read_buffer_rows_filled = 0;
+      u64 file_len = getBlockBytes() * getTotalBlockCount();;
+
+      /* if we've be buffering in-memory, transpose the buffer and flush it */
+      if (read_buffer_rows_filled > 0) {
+        assert(input_width <= read_buffer.n_cols);
+
+        if (flushWriteBufferToTemp(input_height - read_buffer_rows_filled))
+          return -1;
+
+        /*
+        fprintf(stderr, "Flushed last block, file is %ld bytes, "
+                "should be %" PRIu64 "\n", ftell(temp_file), file_len);
+        */
+        assert(ftell(temp_file) == file_len);
+                
       } else {
-        u64 block_count, block_size, file_len;
-        /* If individual lines were being written to the file, the final
-           block may be incomplete. Make sure the block is complete, even
-           if the unneeded data at the end is all zeros. */
-        block_size = (u64)read_buffer.n_rows * read_buffer.n_cols;
-        block_count =
-          (input_width + read_buffer.n_cols - 1) / read_buffer.n_cols;
-        block_count *=
-          (input_height + read_buffer.n_rows - 1) / read_buffer.n_rows;
-        file_len = block_size * block_count;
+
+        /* make sure the final block of data is complete in the temp file,
+           even if the unneeded data at the end is all zeros */
+
+        /* set the file length */
         if (ftruncate(fileno(temp_file), file_len)) {
           fprintf(stderr, "Failed to set length of %s to %" PRIu64
                   " (out of disk space?)\n", temp_file_name, file_len);
           return -1;
         }
+
       }
     }
     return 0;
@@ -522,13 +621,9 @@ int addLine(const char *line, int line_no) {
   if (input_width <= read_buffer.n_cols) {
 
     /* the buffer is full, flush it to the temp file */
-    if (read_buffer_rows_filled == read_buffer.n_rows) {
-      if (insureTempFileExists()) return -1;
-      if (tempFileWrite(line_no - read_buffer_rows_filled, 0, read_buffer.data,
-                        (u64)read_buffer_rows_filled * read_buffer.n_cols))
+    if (read_buffer_rows_filled == read_buffer.n_rows)
+      if (flushWriteBufferToTemp(line_no - read_buffer_rows_filled))
         return -1;
-      read_buffer_rows_filled = 0;
-    }
     
     memcpy(Array2d_ptr(&read_buffer, read_buffer_rows_filled, 0),
            line, input_width);
@@ -552,6 +647,22 @@ int addLine(const char *line, int line_no) {
 }
 
 
+int flushWriteBufferToTemp(int first_row) {
+  if (insureTempFileExists()) return -1;
+
+  transpose(&write_buffer, 0, 0, &read_buffer, 0, 0,
+            read_buffer_rows_filled, input_width);
+
+  if (tempFileWrite(first_row, 0, write_buffer.data, getBlockBytes()))
+    return -1;
+
+  read_buffer_rows_filled = 0;
+
+  return 0;
+}
+
+
+
 /* If the data is all in the read buffer, just transpose it into the write
    buffer. 
 
@@ -559,35 +670,64 @@ int addLine(const char *line, int line_no) {
    block in place.
 */
 int transposeData() {
-  int row, col, height, width;
+  int row, col, height, width, block_no = 0;
   u64 nbytes = (u64)read_buffer.n_rows * read_buffer.n_cols;
+  double time_reading = 0, time_writing = 0, time_transposing = 0, s;
 
   /* if there's a temp file, transpose every block in it */
   if (temp_file) {
+
+    /* shortcut--when reading, if the data was narrower than the read buffer,
+       it was transpose before it was written to the temp file. */
+    if (input_width <= read_buffer.n_cols) return 0;
+
     for (row=0; row < input_height; row += read_buffer.n_rows) {
       height = MIN(read_buffer.n_rows, input_height - row);
       for (col=0; col < input_width; col += read_buffer.n_cols) {
         width = MIN(read_buffer.n_cols, input_width - col);
 
-#if VERBOSE > 1
-        fprintf(stderr, "Transpose block at (%d,%d)\n", row, col);
-#endif
+        if (!quiet) {
+          int blocks = getTotalBlockCount();
+          fprintf(stderr, "\rTranspose block %d of %d, %.1f%% done",
+                  block_no, blocks, 100.0 * block_no / blocks);
+        }
+        block_no++;
+
+        s = getSeconds();
         if (tempFileRead(row, col, read_buffer.data, nbytes))
           return -1;
+        time_reading += getSeconds() - s;
 
+        s = getSeconds();
         transpose(&write_buffer, 0, 0, &read_buffer, 0, 0,
                   height, width);
+        time_transposing += getSeconds() - s;
 
+        s = getSeconds();
         if (tempFileWrite(row, col, write_buffer.data, nbytes))
           return -1;
+        time_writing += getSeconds() - s;
       }
     }
   }
 
   /* the data fit in the read buffer, transpose it into the write buffer */
   else {
+    s = getSeconds();
     transpose(&write_buffer, 0, 0, &read_buffer, 0, 0,
               read_buffer.n_rows, read_buffer.n_cols);
+    time_transposing += getSeconds() - s;
+  }
+
+  if (!quiet) {
+    if (temp_file) {
+      fprintf(stderr, "\r%d blocks transposed, %.3fs reading, "
+              "%.3fs transposing, %.3fs writing\n",
+             getTotalBlockCount(), time_reading, time_transposing, time_writing);
+    } else {
+      fprintf(stderr, "%dx%d transpose time %.3fs\n", input_height, input_width,
+              time_transposing);
+    }
   }
 
   return 0;
@@ -602,6 +742,8 @@ int transposeData() {
 int writeFile(FILE *output_file) {
   int row, col, width, result = 0;
   char *output_row = (char*) malloc(input_height + 2);
+  u64 bytes_written = 0, next_update = IO_UPDATE_FREQUENCY;
+
   if (!output_row) {
     fprintf(stderr, "Out of memory: failed to allocate output row buffer\n");
     return -1;
@@ -623,6 +765,13 @@ int writeFile(FILE *output_file) {
         fprintf(stderr, "Failed to write output row %d (disk full?)\n", row);
         goto fail;
       }
+      bytes_written += input_height + 1;
+
+      if (!quiet && bytes_written > next_update) {
+        fprintf(stderr, "\r%d of %d lines written, %.1f%% done",
+                row, input_width, row * 100.0 / input_width);
+        next_update = bytes_written + IO_UPDATE_FREQUENCY;
+      }
     }
 
   } else {
@@ -633,8 +782,18 @@ int writeFile(FILE *output_file) {
         fprintf(stderr, "Failed to write output row %d (disk full?)\n", row);
         goto fail;
       }
+      bytes_written += input_height + 1;
+
+      if (!quiet && bytes_written > next_update) {
+        fprintf(stderr, "\r%d of %d lines written, %.1f%% done",
+                row, input_width, row * 100.0 / input_width);
+        next_update = bytes_written + IO_UPDATE_FREQUENCY;
+      }
     }
   }
+
+  /* fprintf(stderr, "%" PRIu64 " bytes written\n", bytes_written); */
+  if (!quiet) fputc('\r', stderr);
 
   goto done;
   
